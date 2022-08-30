f8085d31da7fa6f6c67a8b3202085bc72d3f6911
unknown <sfm009@gmail.com>
2022-08-30 14:28:08 +0300

    add_path

--- tasks/pathlib_tasks/add_path.py
+++ tasks/pathlib_tasks/add_path.py
@@ -2,3 +2,11 @@
 Написать функцию add_to_path, которая будет добавлять директорию, в которой находится
 переданный файл (путь к файлу) в sys.path
 """
+import pathlib
+import sys
+
+
+def add_to_path():
+    sys.path.extend([
+        str(pathlib.Path(__file__).parent.resolve())
+    ])
